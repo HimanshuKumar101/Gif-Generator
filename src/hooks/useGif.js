136c4bf1407b584e8db46358bc03f73b25a12cf4
Himanshu Kumar <153725590+HimanshuKumar101@users.noreply.github.com>
2024-10-20 00:14:24 +0530

    Update useGif.js

--- src/hooks/useGif.js
+++ src/hooks/useGif.js
@@ -2,14 +2,13 @@ import React from "react";
 import axios from "axios";
 import { useState, useEffect } from "react";
 
-// API Key should be a string, properly enclosed in quotes or accessed via environment variables.
+
 const API_KEY = process.env.REACT_APP_GIPHY_API_KEY || 'FGJ5I2B9O4LPZRcLZYroLR5SU3arO2Xy';
 const url = `https://api.giphy.com/v1/gifs/random?api_key=${API_KEY}`;
 
 const useGif = (tag) => {
   const [gif, setGif] = useState("");
-  const [loading, setLoading] = useState(false); // use boolean, not string
-
+  const [loading, setLoading] = useState(false); 
   async function fetchData(tag) {
     setLoading(true);
 
